330861ae1345c3611b1c7bd66f13eeebce5aa2e6
Kristen <kristenwood123@gmail.com>
2021-12-09 17:55:32 -0800

    displayed products in store

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -10,20 +10,26 @@ const display = (products, element) => {
             <img src="${image}" class='product-img img' alt="background picture">
             
             <div class="product-icons">
-              <a href="product.html?id=1" class='product-icon'>
+              <a href="product.html?id=${id}" class='product-icon'>
                 <i class="fas fa-search"></i>
               </a>
-              <button class="product-cart-btn product-icon" data-id="1">
+              <button class="product-cart-btn product-icon" data-id="${id}">
                 <i class="fas fa-shopping-cart"></i>
               </button>
             </div>
           </div>
           <footer>
             <p class='product-name'>${name}</p>
-            <h4 class='product-price'>$${price}</h4>
+            <h4 class='product-price'>${formatPrice(price)}</h4>
           </footer>
         </article>`
   }).join('')
+  element.addEventListener('click', function(e) {
+    const parent = e.target.parentElement
+    if(parent.classList.contains('product-cart-btn')){
+        addToCart(parent.dataset.id)
+    }
+  })
 };
 
 export default display;
