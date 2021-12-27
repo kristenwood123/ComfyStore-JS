f78b5e94c47c0f8ca94dfa0f92c9a95c2dd01f0c
Kristen <kristenwood123@gmail.com>
2021-12-27 00:04:27 -0800

    added filter

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,26 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-const setupSearch = () => {};
+
+
+const setupSearch = (store) => {
+  const form = getElement('.input-form')
+  const nameInput = getElement('.search-input')
+
+  form.addEventListener('keyup', function() {
+    const value = nameInput.value;
+    if(value) {
+      const newStore = store.filter((product) => {
+        let { name } = product;
+        name = name.toLowerCase()
+        if(name.startsWith(value)) {
+          return product
+        }
+      })
+      display(newStore, getElement('.products-container'))
+    } else {
+      display(store, getElement('.products-container'))
+    }
+  }) 
+};
 
 export default setupSearch;
